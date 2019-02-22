f1a8fa651f48101eb62362171a700d76915310b4
Aerïa <alexandrebrabant8@gmail.com>
2019-02-22 15:48:23 +0100

    Oui

---
+++ ae.php
@@ -0,0 +1,10 @@
+<html>
+  <?php include('includes/head.php') ?>
+  <body class="ae">
+    <section class="center">
+      <article class="block">
+        <a class="button" href="cassia.php">Suite</a>
+      </article>
+    </section>
+  </body>
+</html>
--- cassia.php
+++ cassia.php
@@ -4,7 +4,8 @@
     <section class="center">
       <article class="block">
         <p>Si vous jouez Cassia et qu'une Auriel vous ressucite,<br>devez-vous retourner directement en 1V5 ?</p>
-        <a class="button" href="auriel.php">Envoyer</a>
+        <a class="button" href="index.php">Oui</a>
+        <a class="button" href="Auriel.php">Non</a>
       </article>
     </section>
   </body>
