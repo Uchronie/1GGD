3084d57c998b83af4bf7f3912bd1a28d10056368
quentin <killfaction@hotmail.fr>
2019-02-22 18:52:02 +0100

    b

--- falseend.php
+++ falseend.php
@@ -3,6 +3,7 @@
   <body class="intro">
     <section class="center">
       <article class="block">
+      	<p> Merci sujet 42, vous pouvez retournez à votre cellule </p>
         <a class="button btn-ae btn-grey" href="falseending">Suite</a>
       </article>
     </section>
