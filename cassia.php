8be1ac83cb03b3ebd6e73fca633488ee3eaa6b72
quentin <killfaction@hotmail.fr>
2019-02-22 15:26:31 +0100

    page cassia

--- cassia.php
+++ cassia.php
@@ -4,8 +4,7 @@
     <section class="center">
       <article class="block">
         <p>Si vous jouez Cassia et qu'une Auriel vous ressucite,<br>devez-vous retourner directement en 1V5 ?</p>
-        <input class="input" type="text"/>
-        <button class="button" type="submit">Envoyer</button>
+        <a class="button" href="auriel.php">Envoyer</a>
       </article>
     </section>
   </body>
