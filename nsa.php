2ed93349e4a1c0c44eab2bfefff34e655a250375
Aerïa <alexandrebrabant8@gmail.com>
2019-02-22 17:27:02 +0100

    nsa

---
+++ nsa.php
@@ -0,0 +1,12 @@
+<html>
+  <?php include('includes/head.php') ?>
+  <body class="fond">
+    <section class="center">
+      <article class="block">
+        <p>NSA IS WATCHING YOU<br> Continuer ?</p>
+        <a class="button" href="fbi.php">OUI !<br>(j'aime le danger)</a>
+        <a class="button" href="last.php">Non</a>
+      </article>
+    </section>
+  </body>
+</html>
