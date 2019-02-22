252f8801d1e40c8c94a9c992ec8d170e1d90653c
Aerïa <alexandrebrabant8@gmail.com>
2019-02-22 15:19:55 +0100

    Page Auriel

---
+++ Auriel.php
@@ -0,0 +1,12 @@
+<html>
+  <?php include('includes/head.php') ?>
+  <body class="auriel">
+    <section class="center">
+      <article class="block">
+        <p>Bien joué ! <br> Auriel est fier de vous !</p>
+        <input class="input" type="text"/>
+        <button class="button" type="submit">Envoyer</button>
+      </article>
+    </section>
+  </body>
+</html>
