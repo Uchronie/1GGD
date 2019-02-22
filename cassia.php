e1984e16bc959594a496c5cdd654eca3caa89327
quentin <killfaction@hotmail.fr>
2019-02-22 15:19:00 +0100

    page cassia

---
+++ cassia.php
@@ -0,0 +1,12 @@
+<html>
+  <?php include('includes/head.php') ?>
+  <body class="cassia">
+    <section class="center">
+      <article class="block">
+        <p>Si vous jouez Cassia et qu'une Auriel vous ressucite,<br>devez-vous retourner directement en 1V5 ?</p>
+        <input class="input" type="text"/>
+        <button class="button" type="submit">Envoyer</button>
+      </article>
+    </section>
+  </body>
+</html>
