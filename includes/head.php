b4afbf93030c942ff8c05d6385a72858f83720f9
Nie <laurette.mrt@gmail.com>
2019-02-22 15:03:21 +0100

    first page

--- includes/head.php
+++ includes/head.php
@@ -3,5 +3,5 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Escape</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+  <link rel="stylesheet" href="./public/style.css">
 </head>
--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <html>
   <?php include('includes/head.php') ?>
-
+  <body class="intro">
+    <section class="center">
+      <article class="block">
+        <p>Bonjour sujet 42, je vous ai convoqué pour une série de test.<br><br>
+        Quel est votre nom ?</p>
+        <input class="input" type="text"/>
+        <button class="button" type="submit">Envoyer</button>
+      </article>
+    </section>
+  </body>
 </html>
