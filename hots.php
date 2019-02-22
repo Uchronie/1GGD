3517a2182a1d24980a38cfd0097f7b63a6f5f240
quentin <killfaction@hotmail.fr>
2019-02-22 16:23:04 +0100

    hots

---
+++ hots.php
@@ -0,0 +1,14 @@
+<html>
+  <?php include('includes/head.php') ?>
+  <body class="fond">
+    <section class="center">
+      <article class="block">
+        <p>LOL ou DOTA ?</p>
+        <form method="POST" action="enigme2.php">
+          <input name="youtube" class="input" type="text"/>
+          <button class="button" type="submit">Envoyer</button>
+        </form>
+      </article>
+    </section>
+  </body>
+</html>
