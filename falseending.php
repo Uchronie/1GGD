be0bbd5f8d2e7b8b414f4572c70f287cceeac0f3
quentin <killfaction@hotmail.fr>
2019-02-22 18:36:51 +0100

    end

---
+++ falseending.php
@@ -0,0 +1,11 @@
+<html>
+  <?php include('includes/head.php') ?>
+  <body class="intro">
+  	<bgsound src="loose.mp3">
+    <section class="center">
+      <article class="block">
+        <a class="button btn-ae btn-grey" href="falseending">Suite</a>
+      </article>
+    </section>
+  </body>
+</html>
