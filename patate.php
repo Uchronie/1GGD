49ca0b03e1b2790b6d0fa983fbcc100b4d5bb92c
quentin <killfaction@hotmail.fr>
2019-02-22 17:46:30 +0100

    patate

---
+++ patate.php
@@ -0,0 +1,10 @@
+<html>
+  <?php include('includes/head.php') ?>
+  <body class="patate">
+    <section class="center">
+      <article class="block">
+        <a class="button btn-ae btn-grey" href="ctrln.php">Suite</a>
+      </article>
+    </section>
+  </body>
+</html>
